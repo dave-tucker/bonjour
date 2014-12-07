cf929aff291c9902ab5455a9a6d40c2d5345a146
Madhu Venugopal <madhu@socketplane.io>
2014-12-07 06:02:45 -0800

    Added a generic Bonjour Daemon to the project which can be reused by the applications
    Hence replaced the mdns_daemon with bonjour_daemon example
    
    Signed-off-by: Madhu Venugopal <madhu@socketplane.io>

---
+++ example/bonjour_daemon.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/socketplane/bonjour"
+)
+
+const DOCKER_CLUSTER_SERVICE = "_foobar._service"
+const DOCKER_CLUSTER_DOMAIN = "local"
+
+func main() {
+	var intfName = ""
+	if len(os.Args) > 1 {
+		intfName = os.Args[1]
+	}
+	b := bonjour.Bonjour{
+		ServiceName:     DOCKER_CLUSTER_SERVICE,
+		ServiceDomain:   DOCKER_CLUSTER_DOMAIN,
+		ServicePort:     9999,
+		InterfaceName:   intfName,
+		OnMemberHello:   newMember,
+		OnMemberGoodBye: removeMember,
+	}
+	b.Start()
+
+	select {}
+}
+
+func newMember(addr net.IP) {
+	fmt.Println("New Member Added : ", addr)
+}
+func removeMember(addr net.IP) {
+	fmt.Println("Member Left : ", addr)
+}
